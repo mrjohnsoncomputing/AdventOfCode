1175df65bf7c1eaa6ecd5069f1881a81e34358a4
MrJohnsonComputing <accounts@mrjohnsoncomputing.co.uk>
2019-05-19 23:11:25 +0100

    Completed Day 6

--- 2015/python/Day6.py
+++ 2015/python/Day6.py
@@ -1,27 +1,32 @@
 #List comprehension
 with open("day6data.txt", "r") as datafile:
     data = [[j for j in i.split(" ") if j != "turn"] for i in datafile.read().split("\n") if i]
-grid = [[0]*1000]
-grid = grid * 1000
+grid = [[0] * 1000 for i in range(1000)]
+bright_grid = [[0] * 1000 for i in range(1000)]
 lightson = 0
+brightness = 0
 for instruction in data:
-    #print(instruction)
     coord_1 = [int(i) for i in instruction[1].split(",")]
     coord_2 = [int(i) for i in instruction[3].split(",")]
-
     action = instruction[0]
-        
-    for y in range(coord_1[1],coord_2[1]):
-        for x in range(coord_1[0],coord_2[0]):
+    for y in range(coord_1[1],coord_2[1]+1):
+        for x in range(coord_1[0],coord_2[0]+1):
             if action == "on":
                 if grid[y][x] == 0:
                     lightson += 1
                 grid[y][x] = 1
+                bright_grid[y][x] += 1
+                brightness += 1
             elif action == "off":
                 if grid[y][x] == 1:
                     lightson -= 1 
                 grid[y][x] = 0
+                if bright_grid[y][x] > 0:
+                    bright_grid[y][x] -= 1
+                    brightness -= 1
             elif action == "toggle":
+                brightness += 2
+                bright_grid[y][x] += 2
                 if grid[y][x] == 1:
                     grid[y][x] = 0
                     lightson -= 1
@@ -32,12 +37,6 @@ for instruction in data:
                     print("Toggle Error")
             else:
                 print("Action error")
-    #print(lightson, action)
+print("There are", lightson, "lights on.")
+print("Brightness:", brightness)
 
-print(lightson)
-lightson = 0
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        if grid[i][j] == 1:
-            lightson+=1
-print(lightson)
