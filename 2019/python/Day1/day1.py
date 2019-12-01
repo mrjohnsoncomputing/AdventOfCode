b0f193e7440cf725ef04e3662c6385915c11ca07
mrjohnsoncomputing <accounts@mrjohnsoncomputing.co.uk>
2019-12-01 22:25:25 +0000

    Created solutions for both parts of Day 1

--- 2019/python/Day1/day1.py
+++ 2019/python/Day1/day1.py
@@ -1,1 +1,34 @@
-print("Advent of Code\n" + "#"*20 + "\nDay 1 Solution")
+import johnson
+
+johnson.welcome(1)
+puzzle_input = johnson.readfile("data.txt")
+
+
+def calculatefuel(weight):
+    total = (int(weight) // 3) - 2
+    if total > 0:
+        return total
+    else:
+        return 0
+
+
+def simplefuel():
+    total = 0
+    for i in range(len(puzzle_input)):
+        total += calculatefuel(puzzle_input[i])
+    return total
+
+
+def recursivefuel():
+    total = 0
+    for i in range(len(puzzle_input)):
+        fuel = calculatefuel(puzzle_input[i])
+        while fuel > 0:
+            total += fuel
+            fuel = calculatefuel(fuel)
+    return total
+
+
+johnson.solution("The sum of the fuel requirements is:", simplefuel())
+johnson.solution("The sum of the corrected fuel requirements is:", recursivefuel())
+
